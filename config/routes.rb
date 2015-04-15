3c0d3d9041b0fc81adda7aa9271382c6f99acf3f
Frank Xu <frankxu@Franks-MacBook-Pro-2.local>
2015-04-15 16:57:49 -0400

    finish exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,18 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pictures#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'pictures' => 'pictures#index'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  post 'pictures' => 'pictures#create'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  get 'pictures/:id/edit' => 'pictures#edit', as: 'edit_picture'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  patch 'pictures/:id' => 'pictures#update'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  get 'pictures/new' => 'pictures#new'
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  delete 'pictures/:id' => 'pictures#destroy', as: 'delete_picture'
 end
